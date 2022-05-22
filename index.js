42007ee324ff08f6577c95c5e64402b4bd5ead11
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-05-23 04:00:54 +0600

    products data added to database

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-const express = require('express');
-const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 6060;
 
-app.get("/", async(req,res)=>{
- res.send("Estro Gadget Server is Running")
-})
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hvdeh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db("estroGadget").collection("products");
+
+    app.get("/products",async(req,res)=>{
+      const products = await productsCollection.find({}).toArray();
+      res.send(products);
+    })
+    
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Estro Gadget Server is Running");
+});
 
 app.listen(port);
