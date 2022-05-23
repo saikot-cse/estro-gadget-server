e85e13b7c052e1393af40e64b861403ff8109f6e
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-05-23 20:58:25 +0600

    make admin implemented

--- index.js
+++ index.js
@@ -43,28 +43,28 @@ async function run() {
       const reviews = await reviewsCollection.find({}).toArray();
       res.send(reviews);
     })
-    app.get("/user",async(req,res)=>{
+    app.get("/user",verifyJWT, async(req,res)=>{
       const users = await userCollection.find({}).toArray();
       res.send(users);
     })
 
-    // app.put('/user/admin/:email', verifyJWT, async (req, res) => {
-    //   const email = req.params.email;
-    //   const requester = req.decoded.email;
-    //   const requesterAccount = await userCollection.findOne({ email: requester });
-    //   if (requesterAccount.role === 'admin') {
-    //     const filter = { email: email };
-    //     const updateDoc = {
-    //       $set: { role: 'admin' },
-    //     };
-    //     const result = await userCollection.updateOne(filter, updateDoc);
-    //     res.send(result);
-    //   }
-    //   else{
-    //     res.status(403).send({message: 'forbidden'});
-    //   }
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+      else{
+        res.status(403).send({message: 'forbidden'});
+      }
 
-    // })
+    })
 
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
